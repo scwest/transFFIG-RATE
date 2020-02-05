658d6b4c96885b626353d50ccd0b603ab005f9cc
swest <seanchristianwest@unomaha.edu>
2020-02-05 17:16:38 -0600

    Update gene.py

--- transffig_prep/gene.py
+++ transffig_prep/gene.py
@@ -3,6 +3,8 @@ Sean West
 5 February 2020
 '''
 
+import collections
+
 class Gene():
     def __init__(self, chromosome='', start=0, end=0, trans=collections.defaultdict(str), name='', strand=1):
         self.chromosome = chromosome
