75a1b090986ee63504fe695f719b8750c9a153f7
swest <seanchristianwest@unomaha.edu>
2020-02-05 17:47:19 -0600

    Update report.py

--- transffig_prep/report.py
+++ transffig_prep/report.py
@@ -8,13 +8,13 @@ class Report():
     def run(self, genes):
         print('Number of Genes:\t{}'.format(len(genes)))
         
-        c = sum([1 for gene in genes if len(gene.trans) > 2])
+        c = sum([1 for gene in genes if len(genes[gene].trans) > 2])
         print('Number of Genes with > 2 Transcripts:\t{}'.format(c))
         
         tran2genes = collections.defaultdict(set)
-        for gene in genes:
+        for gene_name in genes:
             for tran in gene.trans:
-                tran2genes[tran].add(gene)
+                tran2genes[tran].add(gene_name)
         print('Number of Transcripts:\t{}'.format(len(tran2genes)))
         print('Number of Transcripts with > 1 gene:\t{}'.format(len([x for x in tran2genes if len(tran2genes[x]) > 1]))) 
         
